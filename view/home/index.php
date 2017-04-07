cf4bae66bed091cd961a861edefc327cb75a2cad
LAPTOP-6I3EU5UJ\Sander van Steenis <99039450@mydavinci.nl>
2017-04-07 12:01:30 +0200

    Update

--- view/home/index.php
+++ view/home/index.php
@@ -1,4 +1,7 @@
 <div class="container">
- <h1>Welkom bij het php-framework.</h1>
+ <h1>Welkom!</h1>
+
+ <h2>Dit is een site waar een verjaardagskalender op staat.</h2>
+ 
  <p>Je bent nu in home/index.</p>
 </div>
