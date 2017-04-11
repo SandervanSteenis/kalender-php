b671de202fd3a06808c7c05d73f1a4218afa6f51
LAPTOP-6I3EU5UJ\Sander van Steenis <99039450@mydavinci.nl>
2017-04-11 11:17:25 +0200

    Update

--- view/templates/header.php
+++ view/templates/header.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Kalender</title>	
-	<link rel="stylesheet" href="<?= URL ?>">
+	<link rel="stylesheet" href="<?= URL ?>public/css/style.css">
 </head>
 <body>
 	<nav>
