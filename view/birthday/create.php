0e444bb7639e2ea4e3a02bbd5d94abc9f01e49bf
LAPTOP-6I3EU5UJ\Sander van Steenis <99039450@mydavinci.nl>
2017-04-11 14:19:27 +0200

    Update

--- view/birthday/create.php
+++ view/birthday/create.php
@@ -1,10 +1,10 @@
 <div class="container">
 	<form action="<?= URL ?>birthday/createSave" method="post">
 	
-		<input type="text" name="person" placeholder="Name | Johan">
-		<input type="text" name="day" placeholder="Day of birth | 06">
-		<input type="text" name="month" placeholder="Month of birth | february">
-		<input type="text" name="year" placeholder="Year of birth | 2000">
+		<input type="text" name="person" placeholder="Name | John">
+		<input type="text" name="day" placeholder="Day of birth | 08">
+		<input type="text" name="month" placeholder="Month of birth | August">
+		<input type="text" name="year" placeholder="Year of birth | 1990">
 		<input type="submit" value="Verzenden">
 	
 	</form>
--- view/templates/header.php
+++ view/templates/header.php
@@ -5,11 +5,11 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Kalender</title>	
 	<link rel="stylesheet" href="<?= URL ?>public/css/style.css">
-</head>
+	<img id="logo" href="<?= URL ?>public/img/icon.png">
 <body>
 	<nav>
 	<ul>
 		<li><a href="<?= URL ?>home/index">Home</a></li>
-		<li><a href="<?= URL ?>birthdays/index">Birthdays</a></li>
+		<li><a href="<?= URL ?>birthday/index">Birthdays</a></li>
 	</ul>
 	</nav>
